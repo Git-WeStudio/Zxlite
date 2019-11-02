6f0728a5859fcfae5eb9cf8552bf21b40a01d748
We Studio <mail.westudio@gmail.com>
2019-11-02 11:47:04 +0800

    2019.11.2

---
+++ app/src/main/java/we/zxlite/activity/InitActivity.kt
@@ -0,0 +1,13 @@
+package we.zxlite.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import we.zxlite.R
+
+class InitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_init)
+    }
+}
