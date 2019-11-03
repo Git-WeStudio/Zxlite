84281a760686722277077500a06f1ece6f76622b
We Studio <mail.westudio@gmail.com>
2019-11-03 12:27:18 +0800

    2019.11.3

---
+++ app/src/main/java/we/zxlite/view/ShadowView.kt
@@ -0,0 +1,29 @@
+package we.zxlite.view
+
+import android.content.Context
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.GradientDrawable.Orientation.TOP_BOTTOM
+import android.graphics.drawable.GradientDrawable.Orientation.BOTTOM_TOP
+import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES.JELLY_BEAN
+import android.util.AttributeSet
+import android.view.View
+import we.zxlite.R
+import we.zxlite.utils.BaseUtils.color
+
+class ShadowView(ctx: Context, attrs: AttributeSet) : View(ctx, attrs) {
+    init {
+        if (SDK_INT >= JELLY_BEAN) {
+            val typedArray = ctx.obtainStyledAttributes(attrs, R.styleable.ShadowView)
+            val isTop = typedArray.getBoolean(R.styleable.ShadowView_isTop, true)
+            this.background = GradientDrawable().apply {
+                colors = intArrayOf(
+                    ctx.color(R.color.colorShadow),
+                    ctx.color(android.R.color.transparent)
+                )
+                orientation = if (isTop) TOP_BOTTOM else BOTTOM_TOP
+            }
+            typedArray.recycle()
+        }
+    }
+}
